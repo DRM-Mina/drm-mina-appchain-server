1f882cd3ee74049e4968eeffecb5a35c398f84e5
kadirchan <kadircanbozkurt1905@gmail.com>
2024-04-21 17:19:39 +0300

    add winston logging to server

---
+++ apps/express/src/logger.ts
@@ -0,0 +1,21 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: { service: "user-service" },
+    transports: [
+        new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+        new winston.transports.File({ filename: "logs/combined.log" }),
+    ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        })
+    );
+}
+
+export default logger;

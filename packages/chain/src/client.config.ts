52f81763c260abca8ca89d86f572ff35567a177a
kadirchan <kadircanbozkurt1905@gmail.com>
2024-05-09 01:14:35 +0300

    update package

--- packages/chain/src/client.config.ts
+++ packages/chain/src/client.config.ts
@@ -4,7 +4,10 @@ import runtime from "./runtime";
 const appChain = ClientAppChain.fromRuntime(runtime.modules);
 
 appChain.configurePartial({
-  Runtime: runtime.config,
+    Runtime: runtime.config,
+    GraphqlClient: {
+        url: "https://drmmina_chain.kadircan.org/graphql",
+    },
 });
 
 export const client = appChain;

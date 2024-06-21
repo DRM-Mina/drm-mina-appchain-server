433a5a9b66acbb6d04798f7dcf4c1f164312b211
kadirchan <kadircanbozkurt1905@gmail.com>
2024-06-21 18:22:37 +0300

    add index.ts

--- packages/chain/src/index.ts
+++ packages/chain/src/index.ts
@@ -4,5 +4,6 @@ import { UInt64 } from "@proto-kit/library";
 
 export * from "./client.config.js";
 export * from "./modules/index.js";
+export * from "./lib/identifiers/index.js";
 
 export { ClientAppChain, ProtokitLibrary, UInt64 as ProtoUInt64 };

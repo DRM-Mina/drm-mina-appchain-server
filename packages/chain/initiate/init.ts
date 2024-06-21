f5870755fe4cf0c75d3cee4a9cdea48cbae0aacc
kadirchan <kadircanbozkurt1905@gmail.com>
2024-06-22 02:20:52 +0300

    new init add fee

--- packages/chain/initiate/init.ts
+++ packages/chain/initiate/init.ts
@@ -1,6 +1,7 @@
 import { PrivateKey, UInt64 } from "o1js";
 import { games } from "./mock.js";
 import { client } from "../src/index.js";
+import { UInt64 as PUInt64 } from "@proto-kit/library";
 
 const startGames = async () => {
     const publisherKey = PrivateKey.random();
@@ -27,6 +28,24 @@ const startGames = async () => {
         tx.transaction = tx.transaction?.sign(publisherKey);
         await tx.send();
     }
+
+    const tx = await client.transaction(publisher, () => {
+        gameToken.initFeeReceiver(publisher);
+    });
+
+    tx.transaction!.nonce = UInt64.from(games.length);
+    tx.transaction = tx.transaction?.sign(publisherKey);
+    await tx.send();
+
+    console.log("Fee receiver initialized: ", publisher.toBase58());
+
+    const tx2 = await client.transaction(publisher, () => {
+        gameToken.setFeeAmount(PUInt64.from(100));
+    });
+
+    tx2.transaction!.nonce = UInt64.from(games.length + 1);
+    tx2.transaction = tx2.transaction?.sign(publisherKey);
+    await tx2.send();
 };
 
 await startGames();
